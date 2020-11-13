f7ea40ebd03b9cec4b8ae8dce706a51eb8ab305d
Juan Sanchez <juansanchez721@outlook.com>
2020-11-13 00:56:23 -0500

    Finished map class.

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -13,31 +13,35 @@ class Map
 
 
     def set(key, value)
+        if @matrix.flatten.include?(key)
 
+                @matrix.each do |subarr|
+                    if subarr[0] == key
+                        subarr[1] = value
+                    end
+                end
+
+        else
+            @matrix << [key,value]
+        end
 
     end
 
     def get(key)
      
-        @matrix.each{ |arr| 
-    
-        if arr[0] == key
-            return true
-        end
-        
-    }
-
-
+        @matrix.each{ |arr| return arr[0] if arr[0] == key }
+        return "no key found."
     end
 
 
     def delete(key)
 
+        @matrix = @matrix.reject {|subarr| subarr[0] == key}
 
     end
 
     def show
-
+        p @matrix
     end
 
 end
@@ -45,4 +49,11 @@ end
 my_map = [["k1", "v1"], ["k2", "v2"], ["k3", "v2"]]
 
 mapmap = Map.new(my_map)
-p mapmap.get("k2")
+mapmap.set("k2", "2020")
+mapmap.set("k4", "777")
+p mapmap
+
+p mapmap.delete("k4")
+p mapmap
+
+mapmap.show
